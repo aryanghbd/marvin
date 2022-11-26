f03862b9e623244b79bc89223804b83691b524cf
aryanghbd <54407419+aryanghbd@users.noreply.github.com>
2022-11-26 17:55:19 +0000

    Full implementation of applycouncillor command.

--- bot.py
+++ bot.py
@@ -9,8 +9,11 @@ TOKEN = "MTA0NjA0ODM0NDE5MzExNDE3Mw.G2tyzb.9-_Dh8HC2QapyTV15A-ZkNxxqVq9UKO6fpDys
 
 client = commands.Bot(command_prefix='$', intents = Intents.all())
 filimemeo = False
+in_prog = False
 
-helper_questions = ["Are you comfortable with triggering topics?", "Are you willing to stay active in order to help people as a councillor?", "Are you aware of most mental disorders?", "Do you have experience with counselling people?", "Are you at least a little familiar to the psychology field?", "Are you able to handle stress/anxiety well?"]
+helper_questions = ["Are you comfortable with triggering topics?", "Are you willing to stay active in order to help people as a councillor?", "Are you aware of most mental disorders?", "Do you have experience with counselling people?", "Are you at least a little familiar to the psychology field?", "Are you able to handle stress/anxiety well?",
+                    "Are you able to keep a positive mood at all times?", "Would you consider your feelings being more important than the person you are and will be helping?", "Do you know any methods to help people who have trauma?",
+                    "Do you track mental health data and is it important to you?"]
 
 
 
@@ -51,33 +54,79 @@ async def toggle_poof_xenophobia(ctx):
 
 @client.command()
 async def applycouncillor(ctx):
-    em = discord.Embed(title="rich embed test", color=discord.Color.from_rgb(30, 74, 213))
-    em.add_field(name="test", value="god damn")
-    await ctx.send(embed=em)
 
-    guild = ctx.message.guild
-    c = discord.utils.get(guild.categories, name = "staff")
-    test_channel = await guild.create_text_channel('councillor test ' + ctx.author.name, category=c)
+    global in_prog
+    if in_prog is True:
+        await ctx.send("Please wait for the current councillor test to conclude")
 
-    questionpool = random.sample(helper_questions, 3)
-    answers = 0
+    else:
+        in_prog = True
+        em = discord.Embed(title="Your Councillor Application", color=discord.Color.from_rgb(30, 74, 213))
+        em.add_field(name="Councillor Test Information", value=ctx.author.name + ", thank you for considering joining the Councillor team! Please proceed to #councillor-test-" + ctx.author.name + " for an evaluation to see if you are a fit!")
+        await ctx.send(embed=em)
 
-    for question in questionpool:
-        em = discord.Embed(title="Helper Quiz", color=discord.Color.from_rgb(30, 74, 213))
-        em.add_field(name="Question: ", value=question)
-        msg = await test_channel.send(embed=em)
-        await msg.add_reaction('✅')
-        await msg.add_reaction('❌')
+        user = ctx.message.author
+        testrole = discord.utils.get(user.guild.roles, name="councillor test candidate")
+        await user.add_roles(testrole)
 
-        while True:
-            try:
-                reaction = await client.wait_for("reaction_add", timeout=120)
+        guild = ctx.message.guild
+        c = discord.utils.get(guild.categories, name = "councillors")
+        test_channel = await guild.create_text_channel('councillor test ' + ctx.author.name, category=c)
 
-                await msg.remove_reaction(reaction, ctx.author)
+        questionpool = random.sample(helper_questions, 5)
+        answers = 0
 
-            except asyncio.TimeoutError:
-                print("Too slow, loser")
-    await test_channel.send("i am here now my nigga")
+        for question in questionpool:
+            em = discord.Embed(title="Helper Quiz", color=discord.Color.from_rgb(30, 74, 213))
+            em.add_field(name="Question: ", value=question)
+            msg = await test_channel.send(embed=em)
+            await msg.add_reaction('\u2705')
+            await msg.add_reaction('\u274c')
+
+            while True:
+                try:
+                    reaction = await client.wait_for("reaction_add", timeout=120)
+
+                    if str(reaction[0]) == '✅':
+                        print("test")
+
+                    answers = answers + 1 if str(reaction[0]) == '\u2705' else answers
+                    print(answers)
+                    print(reaction[0])
+                    print(reaction)
+
+                    break
+
+                except asyncio.TimeoutError:
+                    print("Too slow, loser")
+
+        if answers >= 3:
+            msg = await test_channel.send("Congratulations! You seem to display all the right characteristics to be a great councillor on our server, we have given you the Councillor role, remember that with great power comes with great responsibility! If you are ready to start helping, please react with a tick to this message to acknowledge that this role is dependent on consistently good positive server behaviour, and breaching such conditions is subject to you losing this role.")
+            await msg.add_reaction('\u2705')
+            while True:
+                try:
+                    reaction = await client.wait_for("reaction_add", timeout=120)
+
+                    if str(reaction[0]) == '\u2705':
+                        user = ctx.message.author
+                        role = discord.utils.get(user.guild.roles, name = "councillor")
+                        await user.add_roles(role)
+                        await user.remove_roles(testrole)
+                        await test_channel.delete()
+
+                    break
+
+                except asyncio.TimeoutError:
+                    print("Too slow, loser")
+
+        else:
+            await test_channel.send("You have the mental health knowledge of a self diagnoser")
+            await asyncio.wait(10)
+            await user.remove_roles(testrole)
+            test_channel.delete()
+
+    in_prog = False
+
 
 
 
